68554f1c69bc5fd42ae1cc5173a9925d013211c0
ricardo-lopes130 <ricardo@saelengenharia.com.br>
2024-09-03 18:18:41 -0300

    renomeei

---
+++ listasdeexercicios/src/main/java/com/poo/lista/lista3/Exercicio1.java
@@ -0,0 +1,20 @@
+package com.poo.lista.lista3;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void resolucao3(){
+        Scanner sc = new Scanner(System.in);
+        //Código de resolução:Exercicio1.resolucao3();
+
+        int numero;
+
+        System.out.println("Digite um número em segundos (entre 1 e 59): ");
+        numero = sc.nextInt();
+
+        
+
+
+    sc.close();
+    }
+}
